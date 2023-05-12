18aea68a17a8d0fbcbb4a87dbcb08f790f4fa5d4
dilz64 <akunguaotomatis@gmail.com>
2023-05-12 19:08:44 +0700

    file.write function update

--- media_player.py
+++ media_player.py
@@ -4,4 +4,4 @@ def initial_stream(video_url, video_src):
     str_video_url = str(video_url)
     with open("video.html", "w") as file:
         file.write(f'<iframe allowfullscreen="true" height="100%" mozallowfullscreen="true" src="{video_src}" webkitallowfullscreen="true" width="100%"></iframe>')
-        webbrowser.open("video.html")
+        webbrowser.open("video_src.html")
