c678dd4cf8b623ebe1c4152e844ca82906e38464
dilz64 <85261639+dilz64@users.noreply.github.com>
2023-05-12 18:11:13 +0700

    Delete media_player.py

--- media_player.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-def initial_stream(video_url):
-    str_video_url = str(video_url)
-    with open("video.html", "w") as file:
-        file.write(str_video_url)
-
-        webbrowser.open("video.html")
